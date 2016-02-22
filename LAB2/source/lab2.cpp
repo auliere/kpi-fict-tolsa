f10926ef1017be92d3d85bb9ef94f40e69f0d814
Oleg Pedorenko <olezhkoo@gmail.com>
2016-02-23 01:03:20 +0200

    Added a sample help message function

---
+++ LAB2/source/lab2.cpp
@@ -0,0 +1,37 @@
+/**
+==============   TOLSA   ===================
+===============  LAB 2  ====================
+============= Variant 16 ===================
+====== Author: Oleg Pedorenko, IP-31 =======
+============= FICT, ASOIU ==================
+========== Created on: 23.02.2016 ==========
+============================================
+*/
+
+#include "iostream"
+#include "string"
+
+
+void printHelp(string exeName)
+{
+	cout << "=============   TOLSA   ====================\n" <<
+			"===============  LAB 2  ====================\n" << 
+			"============= Variant 16 ===================\n" <<
+			"====== Author: Oleg Pedorenko, IP-31 =======\n" <<
+			"============= FICT, ASOIU ==================\n" <<
+			"========== Created on: 23.02.2016 ==========\n" <<
+			"============================================\n";
+	cout << endl;
+	cout << "Pass the path to the file with a list of lines to\n" <<
+			"analyze as a command line argument\n";
+	cout << endl;
+	cout << "Example:\n" << programName << " sample.pas\n";
+	cout << endl;
+	cout << "Output: \nAAAABBBBCC\nThis word exists in given language\n";
+	cout << endl;	
+}
+
+int main(int argc, char** argv)
+{
+	
+}
