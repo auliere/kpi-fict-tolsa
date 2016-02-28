3dfb3f489613f450c89e3511cb82bbe8fa303af7
Oleg Pedorenko <olezhkoo@gmail.com>
2016-02-28 19:10:43 +0200

    Added method to check if the grammar is regular

--- LAB3/source/lab3.py
+++ LAB3/source/lab3.py
@@ -45,9 +45,7 @@ def parse_rules(rules):
     and list of produced symbols as value.
     """
     rules_dict = {}
-    print rules
     for rule in rules:
-        print rule
         L, R = parse_rule(rule)
         rules_dict[L] = R
     return rules_dict
@@ -60,7 +58,80 @@ class Grammar:
         self.N = set(N)
         self.P = parse_rules(P)
         self.S = S
+            
+    def is_terminal(self, letter):
+        return letter in self.T
         
+    def is_nonterminal(self, letter):
+        return letter in self.N
+
+    def is_rightRG(self, line):
+        return (self.is_terminal(line[0]) and 
+                self.is_nonterminal(line[1]) and 
+                len(line) == 2)
+                
+    def is_leftRG(self, line):
+        return (self.is_terminal(line[1]) and 
+                self.is_nonterminal(line[0]) and 
+                len(line) == 2)    
+    
+    def is_regular(self, verbose=False):
+        """
+        Check whether a grammar is regular.
+        Return True if a grammar is regular, False otherwise.
+        Taken from http://goo.gl/TVvRm1
+        """
+        regular = True
+        leftRG = False
+        rightRG = False
+        reason = ""
+        wrong_rule = ""
+        for left_side in self.P:
+            if not self.is_nonterminal(left_side):
+                reason += "\tTerminal in the left side of the rule\n"
+                wrong_rule = left_side + " -> " + self.P[left_side][0]
+                regular = False
+            right_sides = self.P[left_side]
+            for right_side in right_sides:
+                if not regular:
+                    if verbose:
+                        print ("Wrong rule: " + wrong_rule + 
+                            "\n" + reason[:-1])
+                    return False                
+                if len(right_side) > 2:
+                    reason += ("\tMore than two symbols in the" + 
+                        " right side of the rule\n")
+                    wrong_rule = left_side + " -> " + right_side
+                    regular = False
+                if len(right_side) == 1:
+                    if self.is_nonterminal(right_side[0]):
+                        reason += ("\tUnexpected nonterminal in the right " +
+                            "side of the rule\n")
+                        wrong_rule = left_side + " -> " + right_side
+                        regular = False
+                    continue
+                if not(leftRG or rightRG):
+                    leftRG = self.is_leftRG(right_side)
+                    rightRG = self.is_rightRG(right_side)
+                    if not(leftRG or rightRG):
+                        reason += ("\tTwo consecutive terminals or " +
+                            "nonterminals\n")
+                        wrong_rule = left_side + " -> " + right_side
+                        regular = False
+                if rightRG:
+                    if not self.is_rightRG(right_side):
+                        reason += ("\tInvalid order for right RG\n")
+                        wrong_rule = left_side + " -> " + right_side
+                        regular = False
+                if leftRG:
+                    if not self.is_leftRG(right_side):
+                        reason += ("\tInvalid order for left RG\n")
+                        wrong_rule = left_side + " -> " + right_side
+                        regular = False
+        if regular and verbose:
+            print str(self) + "\nThe above grammar is a regular grammar"
+        return regular
+    
     def __repr__(self):
         Ts = '('
         for item in self.T:
@@ -82,14 +153,20 @@ class Grammar:
             i = i+1
         Pns = Pns[:-1]
         return "G = {" + Ts + ", " + Ns + ", " + Ps + ", " + self.S + "}\n" + Pns
-        
+    
        
-parser = argparse.ArgumentParser(description = 'Build an automaton for regular grammar.')
-parser.add_argument("-T", nargs='+', required=True, help='List of terminals of a grammar');
-parser.add_argument("-N", nargs='+', required=True, help='List of non-terminals of a grammar');
-parser.add_argument("-P", nargs='+', required=True, help='Rules of production for grammar');
-parser.add_argument("-S", required=True, help='Starting symbol of a grammar');
+parser = argparse.ArgumentParser(
+                    description = 'Build an automaton for regular grammar.')
+parser.add_argument("-T", nargs='+', required=True, 
+                    help='List of terminals of a grammar');
+parser.add_argument("-N", nargs='+', required=True, 
+                    help='List of non-terminals of a grammar');
+parser.add_argument("-P", nargs='+', required=True,
+                    help='Rules of production for grammar');
+parser.add_argument("-S", required=True,
+                    help='Starting symbol of a grammar');
 
 args = parser.parse_args()
 
-print Grammar(T = args.T, N = args.N, P = args.P, S = args.S)
+g = Grammar(T = args.T, N = args.N, P = args.P, S = args.S)
+g.is_regular(verbose=True)
