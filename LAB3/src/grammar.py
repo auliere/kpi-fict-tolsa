02d2edb5aeafc05fab8ffcc3db3694f054c9516f
Oleg Pedorenko <olezhkoo@gmail.com>
2016-02-29 21:46:17 +0200

    Changed verbose output

--- LAB3/src/grammar.py
+++ LAB3/src/grammar.py
@@ -191,10 +191,10 @@ class Grammar:
             if not regular:
                 break
         if regular and verbose:
-            print (str(self) + "\nThe above grammar is a " + 
+            print "\nThe grammar is a " + 
                 self.type + " regular grammar") 
         if not regular and verbose:
-            print str(self) + "\nThe above grammar is not a regular grammar"
+            print "\nThe above grammar is not a regular grammar"
             print wrong_rule 
             print reason[:-1]
         
